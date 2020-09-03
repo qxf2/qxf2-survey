869ad806ba0a434575a9da023ddae2889bddedff
Harsh Bardhan Mishra <erbeusgriffincasper@gmail.com>
2020-09-03 14:59:54 +0530

    Improved PyLint Rating. Refactored the Code

--- main.py
+++ main.py
@@ -1,94 +1,105 @@
 """
-Sample Endpoint for Qxf2 Survey Application
+FastAPI Endpoints for a Sample Message, Database Authentication,
+and adding new Nodes to the Database. 
 """
 
-from fastapi import FastAPI
-from py2neo import Node, Graph, Relationship, PropertyDict
-from decouple import config
-import uvicorn
 import json
+import uvicorn
+from fastapi import FastAPI
+from py2neo import Node, Graph
+from decouple import config
 
-host = config("DATABASE_HOST")
-username = config("DATABASE_USERNAME")
-password = config("DATABASE_PASSWORD")
+# Grabbing environment variables
+HOSTNAME = config("DATABASE_HOST")
+USERNAME = config("DATABASE_USERNAME")
+PASSWORD = config("DATABASE_PASSWORD")
+
+# Authenticating with the Database
 try:
-    graph = Graph(host, auth=(username, password))
-    db_flag=0
+    GRAPH = Graph(HOSTNAME, auth=(USERNAME, PASSWORD))
+    DB_FLAG = 0
 except:
-    db_flag=1
-     
-graph.delete_all()
-question_label = 'Question'
+    DB_FLAG = 1
 
-question_1 = {
-    'label': 'question1',
-    'properties' : {
+# Deleting the existing Nodes in the Database  
+print(DB_FLAG)
+GRAPH.delete_all()
+# Adding Questions and their individual Labels
+QUESTION_LABEL = 'Question'
+
+QUESTION_1 = {
+    'label': QUESTION_LABEL,
+    'properties': {
         'id': 1,
         'title': 'What is your E-Mail?'
     }
 }
 
-question_2 = {
-    'label': 'question2',
-    'properties' : {
+QUESTION_2 = {
+    'label': QUESTION_LABEL,
+    'properties': {
         'id': 2,
         'title': 'Who helped you this week?'
     }
 }
 
-question_3 = {
-    'label': 'question3',
-    'properties' : {
+QUESTION_3 = {
+    'label': QUESTION_LABEL,
+    'properties': {
         'id': 3,
         'title': 'Whom did you help this week?'
     }
 }
 
-question_4 = {
-    'label': 'question4',
-    'properties' : {
+QUESTION_4 = {
+    'label': QUESTION_LABEL,
+    'properties': {
         'id': 4,
         'title': 'What technologies did you learn?'
     }
 }
 
 app = FastAPI()
+
+
 @app.get("/")
 def read_root():
     "Returns the Message for Mock Data"
     return {"msg": "Mock Data for the Survey"}
+
+
 @app.get("/graph")
 def db_auth():
     "Returns if the Database Connection is Successful or not"
-    if db_flag == 0:
+    if DB_FLAG == 0:
         return {"msg": "Database Connection Authenticated"}
-    else: 
+    else:
         return {"msg": "Database Connection Refused"}
-    
+
+
 @app.get("/questions/{question_id}")
-def read_root(question_id: int):
-    question1_node = Node(question_label, **question_1['properties'])
-    question2_node = Node(question_label, **question_2['properties'])
-    question3_node = Node(question_label, **question_3['properties'])
-    question4_node = Node(question_label, **question_4['properties'])
-    
+def get_questions(question_id: int):
+    question1_node = Node(QUESTION_LABEL, **QUESTION_1['properties'])
+    question2_node = Node(QUESTION_LABEL, **QUESTION_2['properties'])
+    question3_node = Node(QUESTION_LABEL, **QUESTION_3['properties'])
+    question4_node = Node(QUESTION_LABEL, **QUESTION_4['properties'])
+
     node_list = [question1_node, question2_node, question3_node, question4_node]
     for node in node_list:
-        graph.create(node)
-    if (question_id == 1):
-        q1 = json.dumps(question_1)
-        return q1
-    elif (question_id == 2):
-        q2 = json.dumps(question_2)
-        return q2
-    elif (question_id == 3):
-        q3 = json.dumps(question_3)
-        return q3 
-    elif (question_id == 4):
-        q4 = json.dumps(question_4)
-        return q4
+        GRAPH.create(node)
+    if question_id == 1:
+        question_response = json.dumps(QUESTION_1)
+    elif question_id == 2:
+        question_response = json.dumps(QUESTION_2)
+    elif question_id == 3:
+        question_response = json.dumps(QUESTION_3)
+    elif question_id == 4:
+        question_response = json.dumps(QUESTION_4)
     else:
         return {"msg": "Not a valid Question ID"}
+
+    return question_response
+
 
 if __name__ == "__main__":
     uvicorn.run(
