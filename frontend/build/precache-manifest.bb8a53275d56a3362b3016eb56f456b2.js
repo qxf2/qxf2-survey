f323904110d8ceaf1ba91346a3e559a5f4c7db61
akkuldn <akkul.dn@qxf2.com>
2021-08-12 13:38:14 +0530

    Updated build

---
+++ frontend/build/precache-manifest.bb8a53275d56a3362b3016eb56f456b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe2e544abdbcb8c48d8b44abb9b96f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0c69928b74be5e08865",
+    "url": "/static/css/2.9cf0b3bf.chunk.css"
+  },
+  {
+    "revision": "eda2bdf239758ba33d98",
+    "url": "/static/css/main.363c7a7a.chunk.css"
+  },
+  {
+    "revision": "e0c69928b74be5e08865",
+    "url": "/static/js/2.81c2fa0c.chunk.js"
+  },
+  {
+    "revision": "254c01d1a3c7d8124adf4c35e46b76e3",
+    "url": "/static/js/2.81c2fa0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eda2bdf239758ba33d98",
+    "url": "/static/js/main.887e9ba4.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b15593be3d4d546535204571c8eddb8f.js"
+  "/precache-manifest.bb8a53275d56a3362b3016eb56f456b2.js"
 );
 
 self.addEventListener('message', (event) => {
