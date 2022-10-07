d7a687966f3051a88afff0f542d8d7706baa7134
akkuldn <akkul.dn@qxf2.com>
2022-10-07 18:09:08 +0530

    updated build

---
+++ frontend/build/precache-manifest.9fba4ee8f2c8d420af2629f916cac07f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "763e6c76c7be12b4f125fe7518fbf779",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e603d8db5755e10f6e6",
+    "url": "/static/css/2.9cf0b3bf.chunk.css"
+  },
+  {
+    "revision": "ac93e80db7c6e1befe43",
+    "url": "/static/css/main.de417e00.chunk.css"
+  },
+  {
+    "revision": "7e603d8db5755e10f6e6",
+    "url": "/static/js/2.afc63e60.chunk.js"
+  },
+  {
+    "revision": "254c01d1a3c7d8124adf4c35e46b76e3",
+    "url": "/static/js/2.afc63e60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac93e80db7c6e1befe43",
+    "url": "/static/js/main.6f8a2108.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
