b4ac9492a8e01f76d3a1a4e3f49a135bdd68acd7
akkuldn <akkul.dn@qxf2.com>
2022-12-15 15:09:27 +0530

    Made changes to fix PR review comments

--- tests/test_responses_between_dates.py
+++ tests/test_responses_between_dates.py
@@ -1,28 +1,26 @@
 """
-Tests for survey API endpoint that fetches all the user responses between two given dates 
+Tests for survey API endpoint '/survey/admin/QElo_filter_response' that fetches all the user responses between two given dates 
 Test API response for different date ranges
-    - Date range whithin which all the test data is present
+    - Date range within which all the test data is present
     - Date range 1 year prior to the date where test data is stored
     - Current date
     - Date range covers first week of test data
     - Date range covers first two weeks of test data
     - Date range from a week prior to the dates where test data is present ,to the week succeeding the last date which has the test date
 """
-import json
 import os
-import pytest
-from unittest.mock import patch
+import json
 import requests
-from decouple import config
+import pytest
+from datetime import date
 from urllib.parse import urljoin
-from datetime import datetime , timedelta, date
-from dateutil.relativedelta import relativedelta, FR
+from decouple import config
 
 URL = config("URL")
 API_KEY = config("API_KEY")
 
 #Get the url for the API endpoint that fetches user responses b/w two given dates
-responses_url = urljoin(URL, "survey/admin/QElo_filter_response")
+RESPONSES_URL = urljoin(URL, "survey/admin/QElo_filter_response")
 
 """
 Test Data:
@@ -65,7 +63,8 @@ TEST_DATA = [
             {"respondent_id": 2, "date": "1970-01-09", "question_no": 1, "answer": "Generous Giver"}
             ]),
 
-            #Date range from a week prior to the dates where test data is present ,to the week succeeding the last date which has the test date
+            #Date range from a week prior to the dates where test data is present,
+            #to the week succeeding the last date which has the test date
             ({"start_date": "1969-12-23","end_date": "1970-01-23"},[
             {"respondent_id": 1, "date": "1970-01-02", "question_no": 2, "answer": "Stingy Taker"},
             {"respondent_id": 1, "date": "1970-01-09", "question_no": 2, "answer": "Stingy Taker"},
@@ -77,21 +76,15 @@ TEST_DATA = [
             ])
             ]
 
-
-
 @pytest.mark.parametrize("date_range,expected_response", TEST_DATA)
 def test_data_between_dateranges(date_range,expected_response):
-    "Test the response data recieved for different date ranges"
-    
+    "Test the response data recieved for different date ranges"    
     #Get the response from API
-    data_date_range = date_range
-    json_data = json.dumps(data_date_range)
-    response = requests.post(responses_url, data = json_data, headers = {'User': API_KEY})
+    response = requests.post(RESPONSES_URL, data = json.dumps(date_range), headers = {'User': API_KEY})
     response_data = response.json()
     #Compare the two response recieved with the actual response
     diff = [i for i in response_data + expected_response if i not in response_data or i not in expected_response]
     result = len(diff) == 0
-
     assert response.status_code == 200
     assert len(response_data) == len(expected_response)
-    assert result == True,f'There are {len(diff)} differences:\n{diff[:5]}'
+    assert result == True,f'There are {len(diff)} differences:\n{diff}'
